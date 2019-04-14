8204f62e177a3a69fd2274bef16b4ce296991e64
hadi bereksi <hadi-ilies.bereksi-reguig@epitech.eu>
2019-04-14 12:26:16 +0200

    connection by users removed

--- src/command/user_command.c
+++ src/command/user_command.c
@@ -22,16 +22,16 @@ void check_user(char **command, int *new_socket, client_t *client)
 {
     char *name = strdup(command[1]);
 
-    for (size_t i = 0; clients_g[i].user != NULL; i++) {
-        if (strcmp(name, clients_g[i].user) == 0) {
+    //for (size_t i = 0; clients_g[i].user != NULL; i++) {
+        //if (strcmp(name, clients_g[i].user) == 0) {
             dprintf(*new_socket, "%s %s\r\n", code_g[12].code, code_g[12].msg);
             client->username = strdup(name);
            //if (strcmp(client->username, "Anonymous") == 0) {
            //     command[1] = "";
            //     check_password(command, new_socket, client);
            // }
-            return;
-        }
-    }
-    dprintf(*new_socket, "%s %s\r\n", code_g[14].code, code_g[14].msg);
+        //    return;
+        //}
+    //}
+    //dprintf(*new_socket, "%s %s\r\n", code_g[14].code, code_g[14].msg);
 }
